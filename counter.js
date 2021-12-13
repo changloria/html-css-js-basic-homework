dc80bf25369367d3117b8ef1301dff3ca94a98ea
Gloria Chan <changl3@oocl.com>
2021-12-13 09:59:06 +0800

    [JS-Counter][Gloria] reformat lines/codes after code review

--- counter.js
+++ counter.js
@@ -1,27 +1,27 @@
-function plusNum(position){
-    if(position.id == "first") {
-      var number = document.getElementById("first");
+function plusNum(numPosition) {
+    if (numPosition.id == "first") {
+        var number = document.getElementById("first");
     } else {
-      var number = document.getElementById("second");
+        var number = document.getElementById("second");
     }
     number.innerHTML = parseInt(number.innerHTML) + 1;
-    doCalculation();
-  }
-  
-  function minusNum(position){
-    if(position.id == "first") {
-      var number = document.getElementById("first");
+    updateSumNumbers();
+}
+
+function minusNum(numPosition) {
+    if (numPosition.id == "first") {
+        var number = document.getElementById("first");
     } else {
-      var number = document.getElementById("second");
+        var number = document.getElementById("second");
     }
     number.innerHTML = parseInt(number.innerHTML) - 1;
-    doCalculation();
-  }
-  
-  function doCalculation(){
+    updateSumNumbers();
+}
+
+function updateSumNumbers() {
     var number1 = document.getElementById("first");
     var number2 = document.getElementById("second");
-    var answer = document.getElementById("answer");
-  
-    answer.innerHTML = parseInt(number1.innerHTML)+parseInt(number2.innerHTML);
-  }
+    var result = document.getElementById("result");
+
+    result.innerHTML = parseInt(number1.innerHTML) + parseInt(number2.innerHTML);
+}
